2f91aec172108fd777d903e889fca554dfc10fe3
Nicolai-98 <nicolai.sandmann@gmx.net>
2025-08-13 20:30:26 +0200

    added ros templates to snippets. disabled copilot suggestions.

--- update-vscode-settings.bat
+++ update-vscode-settings.bat
@@ -7,6 +7,7 @@ set "VS_CODE_PATH=%APPDATA%\Code\User"
 :: Define the source files (current directory)
 set "KEYBINDINGS_SOURCE=%CD%\keybindings.json"
 set "SETTINGS_SOURCE=%CD%\settings.json"
+set "SNIPPETS_SOURCE=%CD%\ros2.code-snippets"
 
 :: Check if the keybindings.json and settings.json exist in the current directory
 if not exist "%KEYBINDINGS_SOURCE%" (
@@ -19,11 +20,19 @@ if not exist "%SETTINGS_SOURCE%" (
     goto end
 )
 
+if not exist "%SNIPPETS_SOURCE%" (
+    echo snippets not found in the current directory.
+    goto end
+)
+
 :: Overwrite the existing keybindings.json and settings.json in VS Code directory
 copy /Y "%KEYBINDINGS_SOURCE%" "%VS_CODE_PATH%\keybindings.json"
 echo keybindings.json has been updated!
 
 copy /Y "%SETTINGS_SOURCE%" "%VS_CODE_PATH%\settings.json"
+echo settings.json has been updated!
+
+copy /Y "%SNIPPETS_SOURCE%" "%VS_CODE_PATH%\ros2.code-snippets"
 echo settings.json has been updated!
 
 :end
--- update-vscode-settings.sh
+++ update-vscode-settings.sh
@@ -6,6 +6,7 @@ VS_CODE_PATH="$HOME/.config/Code/User"
 # Define the source files (current directory)
 KEYBINDINGS_SOURCE="$(pwd)/keybindings.json"
 SETTINGS_SOURCE="$(pwd)/settings.json"
+SNIPPETS_SOURCE="$(pwd)/ros2.code-snippets"
 
 # Check if keybindings.json and settings.json exist in the current directory
 if [[ ! -f "$KEYBINDINGS_SOURCE" ]]; then
@@ -18,9 +19,17 @@ if [[ ! -f "$SETTINGS_SOURCE" ]]; then
     exit 1
 fi
 
+if [[ ! -f "$SNIPPETS_SOURCE" ]]; then
+    echo "snippets not found in the current directory."
+    exit 1
+fi
+
 # Overwrite the existing keybindings.json and settings.json in VS Code directory
 cp -f "$KEYBINDINGS_SOURCE" "$VS_CODE_PATH/keybindings.json"
 echo "keybindings.json has been updated!"
 
 cp -f "$SETTINGS_SOURCE" "$VS_CODE_PATH/settings.json"
 echo "settings.json has been updated!"
+
+cp -f "$SNIPPETS_SOURCE" "$VS_CODE_PATH/snippets/ros2.code-snippets"
+echo "code snippets have been updated!"
